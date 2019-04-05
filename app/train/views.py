38facd4817ff61d684ba71dcffc26f209e18177d
Andrew Syrmakesis <siarziss@gmail.com>
2019-04-05 16:34:12 +0300

    added train progress bar (not working)

--- app/train/views.py
+++ app/train/views.py
@@ -6,20 +6,6 @@ from OriginalModel.offline_process import model_train
 
 # Create your views here.
 def index(request):
-    path_nwp = r'C:\Users\User\WorkSpace\ForecastApp\OriginalModel\forecasting_platform\Nwp_2'
-    path_data_train = r'C:\Users\User\WorkSpace\ForecastApp\OriginalModel\forecasting_platform\Data\offline\off_data_train.csv'
-    path_data_evaluation = r'C:\Users\User\WorkSpace\ForecastApp\OriginalModel\forecasting_platform\Data\offline\off_data_evaluation.csv'
-
-    nominal_p = 28000
-    horizon = 48
-
-    # Training
-
-    start_date_train = '01/01/2011'
-    end_date_train = '10/30/2011'
-
-    # mdl_train = model_train(path_data_train, path_nwp, nominal_p, horizon, start_date_train, end_date_train)
-    # win_df, wout_df, bin_df, bout_df = mdl_train.train(h=50, epochs=50)
 
     context = {
         'user_name': 'Andrew Syrmakesis',
@@ -29,5 +15,24 @@ def index(request):
         'ordered_warranty': True
     }
 
-    # return HttpResponse('Congratulations. Training was completed!')
     return render(request, 'train/index.html', context)
+
+
+def train(request):
+
+    path_nwp = r'C:\Users\User\WorkSpace\ForecastApp\OriginalModel\forecasting_platform\Nwp_2'
+    path_data_train = r'C:\Users\User\WorkSpace\ForecastApp\OriginalModel\forecasting_platform\Data\offline\off_data_train.csv'
+
+    nominal_p = 28000
+    horizon = 48
+
+    start_date_train = '01/01/2011'
+    end_date_train = '10/30/2011'
+
+    mdl_train = model_train(path_data_train, path_nwp, nominal_p, horizon, start_date_train, end_date_train)
+    win_df, wout_df, bin_df, bout_df = mdl_train.train(h=50, epochs=50)
+
+    context = {
+    }
+
+    return render(request, 'train/train.html', context)

045c46aba0108974ba484c3d95ae156d4765683b
Andrew Syrmakesis <siarziss@gmail.com>
2019-04-08 16:28:24 +0300

    introduced progress bar with celery (not working)

--- app/train/views.py
+++ app/train/views.py
@@ -20,8 +20,8 @@ def index(request):
 
 def train(request):
 
-    path_nwp = r'C:\Users\User\WorkSpace\ForecastApp\OriginalModel\forecasting_platform\Nwp_2'
-    path_data_train = r'C:\Users\User\WorkSpace\ForecastApp\OriginalModel\forecasting_platform\Data\offline\off_data_train.csv'
+    path_nwp = r'C:\Users\User\WorkSpace\ForecastApp\app\OriginalModel\forecasting_platform\Nwp_2'
+    path_data_train = r'C:\Users\User\WorkSpace\ForecastApp\app\OriginalModel\forecasting_platform\Data\offline\off_data_train.csv'
 
     nominal_p = 28000
     horizon = 48
@@ -29,8 +29,11 @@ def train(request):
     start_date_train = '01/01/2011'
     end_date_train = '10/30/2011'
 
-    mdl_train = model_train(path_data_train, path_nwp, nominal_p, horizon, start_date_train, end_date_train)
-    win_df, wout_df, bin_df, bout_df = mdl_train.train(h=50, epochs=50)
+    h = 50
+    epochs = 50
+
+    mdl_train = model_train()
+    win_df, wout_df, bin_df, bout_df = mdl_train.delay(path_data_train, path_nwp, nominal_p, horizon, start_date_train, end_date_train, h, epochs)
 
     context = {
     }
